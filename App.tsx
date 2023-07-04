f7bffd24a9348f86f48141ec7a32d6e55576954b
Lucas Castilho <lucascastilholiveira@gmail.com>
2023-07-04 11:22:57 -0300

    chore: configures theme

--- App.tsx
+++ App.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import {SafeAreaView} from 'react-native';
+import {ThemeProvider} from '@shopify/restyle';
+
 import {Text} from './src/components/Text';
+import {Button} from './src/components/Button';
+import {theme} from './src/theme/theme';
 
 function App(): JSX.Element {
   return (
-    <SafeAreaView>
-      <Text preset="headingLarge">Teste de fonte</Text>
-      <Text preset="headingMedium">Teste de fonte</Text>
-    </SafeAreaView>
+    <ThemeProvider theme={theme}>
+      <SafeAreaView>
+        <Text preset="headingLarge">Teste de fonte</Text>
+        <Text preset="headingMedium">Teste de fonte</Text>
+        <Button title="Entrar" backgroundColor="carrotSecondary" mt="s20" />
+      </SafeAreaView>
+    </ThemeProvider>
   );
 }
 
