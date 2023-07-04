14100d653d51e31a5deb0ebef2666e4d4f419bbf
Lucas Castilho <lucascastilholiveira@gmail.com>
2023-07-04 11:39:11 -0300

    feat: creates box component

---
+++ src/components/Box/index.tsx
@@ -0,0 +1,34 @@
+import {
+  createBox,
+  createRestyleComponent,
+  backgroundColor,
+  BackgroundColorProps,
+  spacing,
+  SpacingProps,
+  layout,
+  LayoutProps,
+  border,
+  BorderProps,
+  spacingShorthand,
+  SpacingShorthandProps,
+} from '@shopify/restyle';
+import {Theme} from '../../theme/theme';
+import {TouchableOpacity, TouchableOpacityProps} from 'react-native';
+
+export const Box = createBox<Theme>();
+export type BoxPropx = React.ComponentProps<typeof Box>;
+
+export type TouchableOpacityBoxProps = TouchableOpacityProps &
+  BackgroundColorProps<Theme> &
+  SpacingProps<Theme> &
+  LayoutProps<Theme> &
+  BorderProps<Theme> &
+  SpacingShorthandProps<Theme>;
+
+export const TouchableOpacityBox = createRestyleComponent<
+  TouchableOpacityBoxProps,
+  Theme
+>(
+  [backgroundColor, spacing, layout, border, spacingShorthand],
+  TouchableOpacity,
+);
