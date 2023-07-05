3011f351c108b42339d0d3e4e14fe80607fae57b
Lucas Castilho <lucascastilholiveira@gmail.com>
2023-07-05 12:20:50 -0300

    refactor: changes theme hook

--- src/components/ActivityIndicator/index.tsx
+++ src/components/ActivityIndicator/index.tsx
@@ -2,9 +2,8 @@ import {
   ActivityIndicator as RNActivityIndicator,
   ActivityIndicatorProps as RNActivityIndicatorProps,
 } from 'react-native';
-import React from 'react';
-import {useTheme} from '@shopify/restyle';
-import {Theme, ThemeColors} from '../../theme/theme';
+import {ThemeColors} from '../../theme/theme';
+import {useAppTheme} from '../../hooks/useAppTheme';
 
 export interface ActivityIndicatorProps
   extends Omit<RNActivityIndicatorProps, 'color'> {
@@ -15,7 +14,7 @@ export function ActivityIndicator({
   color,
   ...rnActivityIndicatorProps
 }: ActivityIndicatorProps) {
-  const {colors} = useTheme<Theme>();
+  const {colors} = useAppTheme();
 
   return (
     <RNActivityIndicator color={colors[color]} {...rnActivityIndicatorProps} />
