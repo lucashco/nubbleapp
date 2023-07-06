6d4b46bb15a4ee1c8817db890890e8897adcdcfd
Lucas Castilho <lucascastilholiveira@gmail.com>
2023-07-06 13:02:41 -0300

    feat: creates a Sign up screen

---
+++ src/screens/auth/SignUpScreen/SignUpScreen.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import {Screen} from '../../../components/Screen/Screen';
+import {Text} from '../../../components/Text';
+import {TextInput} from '../../../components/TextInput';
+import {Button} from '../../../components/Button';
+
+export function SignUpScreen() {
+  function submitForm() {
+    //TODO - implement
+  }
+  return (
+    <Screen canGoBack isScrollable>
+      <Text preset="headingLarge" mb="s32" bold>
+        Criar uma conta
+      </Text>
+
+      <TextInput label="Seu username" placeholder="@" boxProps={{mb: 's20'}} />
+      <TextInput
+        label="Nome completo"
+        placeholder="Digite seu nome"
+        boxProps={{mb: 's20'}}
+      />
+      <TextInput
+        label="E-mail"
+        placeholder="Digite seu e-mail"
+        keyboardType="email-address"
+        boxProps={{mb: 's20'}}
+      />
+      <TextInput
+        label="Senha"
+        placeholder="Digite sua senha"
+        secureTextEntry
+        boxProps={{mb: 's48'}}
+      />
+
+      <Button onPress={submitForm} title="Criar minha conta" />
+    </Screen>
+  );
+}
