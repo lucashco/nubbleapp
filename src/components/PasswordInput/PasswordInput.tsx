30fdd026f5906bdd6d2cf32aa85c06ee5774c7ac
Lucas Castilho <lucascastilholiveira@gmail.com>
2023-07-06 13:39:53 -0300

    feat: creates a password input

---
+++ src/components/PasswordInput/PasswordInput.tsx
@@ -0,0 +1,27 @@
+import React, {useState} from 'react';
+import {TextInput, TextInputProps} from '../TextInput';
+import {Icon} from '../Icon';
+
+type PasswordInputProps = Omit<TextInputProps, 'RightComponent'>;
+
+export default function PasswordInput(props: PasswordInputProps) {
+  const [isSecureText, setIsSecureText] = useState(true);
+
+  function toggleSecureTextEntry() {
+    setIsSecureText(prevState => !prevState);
+  }
+
+  return (
+    <TextInput
+      secureTextEntry={isSecureText}
+      {...props}
+      RightComponent={
+        <Icon
+          onPress={toggleSecureTextEntry}
+          name={isSecureText ? 'eyeOn' : 'eyeOff'}
+          color="gray2"
+        />
+      }
+    />
+  );
+}
