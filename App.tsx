1e739e2dbf5bad620c00d9d6675db3eb87dfed68
Lucas Castilho <lucascastilholiveira@gmail.com>
2023-07-10 14:48:20 -0300

    feat: configures route

--- App.tsx
+++ App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import {ThemeProvider} from '@shopify/restyle';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
-import {LoginScreen} from './src/screens/auth/LoginScreen/LoginScreen';
+import {Router} from './src/routes/routes';
+
 import {theme} from './src/theme/theme';
 
 function App(): JSX.Element {
   return (
-    <ThemeProvider theme={theme}>
-      <LoginScreen />
-    </ThemeProvider>
+    <SafeAreaProvider>
+      <ThemeProvider theme={theme}>
+        <Router />
+      </ThemeProvider>
+    </SafeAreaProvider>
   );
 }
 
