e135fed575acf90aef48702eb24b294ff37359a8
Lucas Castilho <lucascastilholiveira@gmail.com>
2023-07-17 18:19:43 -0300

    feature: creates types

--- src/routes/AppStack.tsx
+++ src/routes/AppStack.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import {NavigatorScreenParams} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-import {HomeScreen, SettingsScreen} from '@screens';
+import {SettingsScreen} from '@screens';
+
+import {AppBottomTabParamList, AppTabNavigator} from './AppTabNavigator';
 
 export type AppStackParamList = {
-  HomeScreen: undefined;
+  AppTabNavigator: NavigatorScreenParams<AppBottomTabParamList>;
   SettingsScreen: undefined;
 };
 
@@ -18,7 +21,7 @@ export function AppStack() {
         headerShown: false,
         fullScreenGestureEnabled: true,
       }}>
-      <Stack.Screen name="HomeScreen" component={HomeScreen} />
+      <Stack.Screen name="AppTabNavigator" component={AppTabNavigator} />
       <Stack.Screen name="SettingsScreen" component={SettingsScreen} />
     </Stack.Navigator>
   );
