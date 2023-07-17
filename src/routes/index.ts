8dd9c9bfd8bd223ce12d6e8bc028181418e20413
Lucas Castilho <lucascastilholiveira@gmail.com>
2023-07-17 17:08:19 -0300

    refactor: creates a generic screen props

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,2 +1,2 @@
 export * from './Routes';
-export * from './AppStack';
+export * from './navigationType';
--- src/screens/app/SettingsScreen/SettingsScreen.tsx
+++ src/screens/app/SettingsScreen/SettingsScreen.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
 
-import {NativeStackScreenProps} from '@react-navigation/native-stack';
-
 import {Button, Screen, Text} from '@components';
-import {AppStackParamList} from '@routes';
+import {AppScreenProps} from '@routes';
 
-type ScreenProps = NativeStackScreenProps<AppStackParamList, 'SettingsScreen'>;
-
-export function SettingsScreen({}: ScreenProps) {
+export function SettingsScreen({}: AppScreenProps<'SettingsScreen'>) {
   return (
     <Screen canGoBack>
       <Text>SETTINGS SCREEN</Text>
