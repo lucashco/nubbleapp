9614d66d21524f5d703db1a14f7af1578df9a58a
Lucas Castilho <lucascastilholiveira@gmail.com>
2023-07-29 08:52:50 -0300

    feat: creates post adapter

---
+++ src/domain/Post/postAdapter.ts
@@ -0,0 +1,24 @@
+import {Post, PostAPI} from './postTypes';
+
+/**
+ * @description Adapta o PostAPI para o modelo de Post.
+ */
+function toPost(postAPI: PostAPI): Post {
+  return {
+    id: postAPI.id.toString(),
+    text: postAPI.text,
+    author: {
+      name: postAPI.user.full_name,
+      userName: postAPI.user.username,
+      profileURL: postAPI.user.profile_url,
+    },
+    imageURL: postAPI.image_url,
+    commentCount: parseInt(postAPI.meta.comments_count, 10),
+    favoriteCount: parseInt(postAPI.meta.favorite_count, 10),
+    reactionCount: parseInt(postAPI.meta.like_count, 10),
+  };
+}
+
+export const postAdapter = {
+  toPost,
+};
