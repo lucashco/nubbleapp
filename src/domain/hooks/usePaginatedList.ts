64df7c28f7dca21411fb4f5518b04b9eadbec129
Lucas Castilho <lucascastilholiveira@gmail.com>
2023-08-01 16:07:26 -0300

    refactor: abstracts pagination to hook

---
+++ src/domain/hooks/usePaginatedList.ts
@@ -0,0 +1,65 @@
+import {useState, useEffect} from 'react';
+
+import {Page} from '@types';
+
+export function usePaginatedList<Data>(
+  getList: (page: number) => Promise<Page<Data>>,
+) {
+  const [postList, setPostList] = useState<Data[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [page, setPage] = useState(1);
+  const [hasNextPage, setHasNextPage] = useState(true);
+
+  async function fetchInitialData() {
+    try {
+      setError(false);
+      setLoading(true);
+      const {data, meta} = await getList(1);
+      setPostList(data);
+      if (meta.hasNextPage) {
+        setPage(currentPage => currentPage + 1);
+      } else {
+        setHasNextPage(false);
+      }
+    } catch (err) {
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  async function fetchNextPage() {
+    if (loading || !hasNextPage) {
+      return;
+    }
+
+    try {
+      setLoading(true);
+      const {data, meta} = await getList(page);
+      setPostList(currentList => currentList.concat(data));
+      if (meta.hasNextPage) {
+        setPage(currentPage => currentPage + 1);
+      } else {
+        setHasNextPage(false);
+      }
+    } catch (err) {
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchInitialData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return {
+    postList,
+    error,
+    loading,
+    refresh: fetchInitialData,
+    fetchNextPage,
+  };
+}
