e56ea26f8daa3e944b62464cf0410d72c4788280
Lucas Castilho <lucascastilholiveira@gmail.com>
2023-08-01 16:29:02 -0300

    chore: fixes the user name and abstract style

--- src/components/PostItem/components/PostHeader.tsx
+++ src/components/PostItem/components/PostHeader.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Image} from 'react-native';
+import {Image, ImageStyle, StyleProp} from 'react-native';
 
 import {Post} from '@domain';
 
@@ -10,13 +10,16 @@ type PostHeaderProps = Pick<Post, 'author'>;
 export function PostHeader({author}: PostHeaderProps) {
   return (
     <Box flexDirection="row" alignItems="center" mb="s16">
-      <Image
-        source={{uri: author.profileURL}}
-        style={{width: 32, height: 32, borderRadius: 14}}
-      />
+      <Image source={{uri: author.profileURL}} style={$imageStyle} />
       <Text preset="paragraphMedium" semiBold ml="s12">
-        {author.name}
+        {author.userName}
       </Text>
     </Box>
   );
 }
+
+const $imageStyle: StyleProp<ImageStyle> = {
+  width: 32,
+  height: 32,
+  borderRadius: 14,
+};
