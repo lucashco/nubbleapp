d8476af74ec4375c48d87b980b72efd8cd0842b8
Lucas Castilho <lucascastilholiveira@gmail.com>
2023-08-01 16:22:36 -0300

    feat: creates comments domain

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -3,5 +3,7 @@ export * from './Post/postService';
 export * from './Post/useCases/usePostList';
 
 export * from './PostComment/postCommentTypes';
+export * from './PostComment/postCommentService';
+export * from './PostComment/useCases/usePostCommentList';
 
 export * from './hooks/usePaginatedList';
