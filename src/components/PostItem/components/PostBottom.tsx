265c0c2ef41014133c16aa814e99314412e2609c
Lucas Castilho <lucascastilholiveira@gmail.com>
2023-08-02 11:50:46 -0300

    refactor: repasses post id props

--- src/components/PostItem/components/PostBottom.tsx
+++ src/components/PostItem/components/PostBottom.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
 
 import {Post} from '@domain';
+import {useNavigation} from '@react-navigation/native';
 
 import {Box, BoxProps, Text} from '@components';
 
-type PostBottomProps = Pick<Post, 'text' | 'author' | 'commentCount'> &
+type PostBottomProps = Pick<Post, 'id' | 'text' | 'author' | 'commentCount'> &
   BoxProps;
 
 export function PostBottom({
   author,
   text,
   commentCount,
+  id,
   ...rest
 }: PostBottomProps) {
+  const navigation = useNavigation();
   const commentText = getCommentText(commentCount);
+
+  function navigateToPostCommentScreen() {
+    navigation.navigate('PostCommentScreen', {
+      postId: id,
+    });
+  }
 
   return (
     <Box {...rest}>
@@ -24,7 +33,12 @@ export function PostBottom({
         {text}
       </Text>
       {commentText && (
-        <Text bold color="primary" preset="paragraphSmall" mt="s8">
+        <Text
+          onPress={navigateToPostCommentScreen}
+          bold
+          color="primary"
+          preset="paragraphSmall"
+          mt="s8">
           {commentText}
         </Text>
       )}
