e765bd0aa4ffb4770d5e349b739188056d7d4557
Lucas Castilho <lucascastilholiveira@gmail.com>
2023-08-03 14:52:00 -0300

    feat: creates text message component

---
+++ src/components/TextMessage/TextMessage.tsx
@@ -0,0 +1,69 @@
+import React, {useRef} from 'react';
+import {
+  Pressable,
+  TextInput as RNTextInput,
+  TextInputProps as RNTextInputProps,
+  TextStyle,
+} from 'react-native';
+
+import {useAppTheme} from '@hooks';
+
+import {Box} from '../Box/Box';
+import {$fontFamily, $fontSizes, Text} from '../Text/Text';
+
+interface TextMessageProps extends RNTextInputProps {
+  onPressSend: () => void;
+}
+
+export function TextMessage({
+  onPressSend,
+  value,
+  ...rnTextInputProps
+}: TextMessageProps) {
+  const {colors} = useAppTheme();
+  const inputRef = useRef<RNTextInput>(null);
+
+  function focusInput() {
+    inputRef.current?.focus();
+  }
+
+  const sendIsDisabled = value?.trim().length === 0;
+
+  return (
+    <Pressable onPress={focusInput}>
+      <Box
+        paddingHorizontal="s16"
+        paddingVertical="s14"
+        backgroundColor="gray5"
+        flexDirection="row"
+        justifyContent="space-between"
+        alignItems="center"
+        borderRadius="s12">
+        <RNTextInput
+          ref={inputRef}
+          value={value}
+          placeholderTextColor={colors.gray2}
+          style={[$textInputStyle, {color: colors.gray1}]}
+          {...rnTextInputProps}
+        />
+      </Box>
+
+      <Pressable onPress={onPressSend} disabled={sendIsDisabled}>
+        <Text
+          preset="paragraphMedium"
+          color={sendIsDisabled ? 'gray2' : 'primary'}>
+          Enviar
+        </Text>
+      </Pressable>
+    </Pressable>
+  );
+}
+
+const $textInputStyle: TextStyle = {
+  padding: 0,
+  flexGrow: 1,
+  flexShrink: 1,
+
+  fontFamily: $fontFamily.regular,
+  ...$fontSizes.paragraphMedium,
+};
