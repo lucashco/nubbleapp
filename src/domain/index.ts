c9578a8edc23fa5b14a3df6075dec2a49ee2be6b
Lucas Castilho <lucascastilholiveira@gmail.com>
2023-08-03 07:58:59 -0300

    refactor: imports refact

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -2,8 +2,5 @@ export * from './Post/postTypes';
 export * from './Post/postService';
 export * from './Post/useCases/usePostList';
 
-export * from './PostComment/postCommentTypes';
-export * from './PostComment/postCommentService';
-export * from './PostComment/useCases/usePostCommentList';
-
+export * from './PostComment';
 export * from './hooks/usePaginatedList';
