97cdf55fb75b90fac5844085b6d83e05c8a6b336
Lucas Castilho <lucascastilholiveira@gmail.com>
2023-08-03 08:46:38 -0300

    chore: abstract profile image to component

--- src/components/PostItem/components/PostHeader.tsx
+++ src/components/PostItem/components/PostHeader.tsx
@@ -1,25 +1,18 @@
 import React from 'react';
-import {Image, ImageStyle, StyleProp} from 'react-native';
 
 import {Post} from '@domain';
 
-import {Box, Text} from '@components';
+import {Box, ProfileAvatar, Text} from '@components';
 
 type PostHeaderProps = Pick<Post, 'author'>;
 
 export function PostHeader({author}: PostHeaderProps) {
   return (
     <Box flexDirection="row" alignItems="center" mb="s16">
-      <Image source={{uri: author.profileURL}} style={$imageStyle} />
+      <ProfileAvatar imageURL={author.profileURL} />
       <Text preset="paragraphMedium" semiBold ml="s12">
         {author.userName}
       </Text>
     </Box>
   );
 }
-
-const $imageStyle: StyleProp<ImageStyle> = {
-  width: 32,
-  height: 32,
-  borderRadius: 14,
-};
