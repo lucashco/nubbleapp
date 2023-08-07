c3c3d26355c3cf6cf136dad987d395dc278ea4d4
Lucas Castilho <lucascastilholiveira@gmail.com>
2023-08-07 08:41:32 -0300

    refactor: abstracts mutation logic to hook

---
+++ src/infra/hooks/useMutation.ts
@@ -0,0 +1,41 @@
+import {useState} from 'react';
+
+export interface MutationOptions<TData> {
+  onSuccess?: (data: TData) => void;
+  onError?: (message: string) => void;
+  errorMessage?: string;
+}
+
+export function useMutation<TVariables, TData>(
+  mutationFn: (variables: TVariables) => Promise<TData>,
+  options?: MutationOptions<TData>,
+) {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<boolean | null>(null);
+
+  async function mutate(variables: TVariables) {
+    try {
+      setError(null);
+      setLoading(true);
+
+      const data = await mutationFn(variables);
+
+      if (options?.onSuccess) {
+        options.onSuccess(data);
+      }
+    } catch (err) {
+      if (options?.onError) {
+        options.onError(options.errorMessage || '');
+      }
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return {
+    mutate,
+    loading,
+    error,
+  };
+}
