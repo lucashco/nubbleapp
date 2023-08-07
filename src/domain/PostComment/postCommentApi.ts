2c52cf81079dc5d40e6229402fc89a1e4cbf95db
Lucas Castilho <lucascastilholiveira@gmail.com>
2023-08-07 07:53:45 -0300

    feat: creates service to remove comment

--- src/domain/PostComment/postCommentApi.ts
+++ src/domain/PostComment/postCommentApi.ts
@@ -2,16 +2,21 @@ import {PageAPI, PageParams, api} from '@api';
 
 import {PostCommentApi} from './postCommentTypes';
 
+const POST_COMMENT_BASE_URL = 'user/post_comment';
+
 async function getList(
   post_id: number,
   pageParams?: PageParams,
 ): Promise<PageAPI<PostCommentApi>> {
-  const response = await api.get<PageAPI<PostCommentApi>>('user/post_comment', {
-    params: {
-      post_id,
-      ...pageParams,
+  const response = await api.get<PageAPI<PostCommentApi>>(
+    POST_COMMENT_BASE_URL,
+    {
+      params: {
+        post_id,
+        ...pageParams,
+      },
     },
-  });
+  );
 
   return response.data;
 }
@@ -20,7 +25,7 @@ async function create(
   post_id: number,
   message: string,
 ): Promise<PostCommentApi> {
-  const response = await api.post<PostCommentApi>('user/post_comment', {
+  const response = await api.post<PostCommentApi>(POST_COMMENT_BASE_URL, {
     post_id,
     message,
   });
@@ -28,7 +33,16 @@ async function create(
   return response.data;
 }
 
+async function remove(postCommentId: number): Promise<{message: string}> {
+  const response = await api.delete<{message: string}>(
+    `${POST_COMMENT_BASE_URL}/${postCommentId}`,
+  );
+
+  return response.data;
+}
+
 export const postCommentApi = {
   getList,
   create,
+  remove,
 };
