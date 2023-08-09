f9d76d185d156669637785052c47cba05e7fde16
Lucas Castilho <lucascastilholiveira@gmail.com>
2023-08-09 09:08:14 -0300

    feat: creates remove comment use case

--- src/domain/PostComment/postCommentService.ts
+++ src/domain/PostComment/postCommentService.ts
@@ -33,9 +33,32 @@ async function remove(postCommentId: number): Promise<string> {
 
   return response.message;
 }
+/**
+ * @description The user can delete the comment if he is the author of the post or the author of the comment.
+ * @param userId the user id of current session
+ * @param postComment the comment to be deleted
+ * @param postAuthorId the id of the author of the post
+ */
+
+function isAllowToDelete(
+  postComment: PostComment,
+  userId: number,
+  postAuthorId: number,
+): boolean {
+  if (postComment.author.id === userId) {
+    return true;
+  }
+
+  if (postAuthorId === userId) {
+    return true;
+  }
+
+  return false;
+}
 
 export const postCommentService = {
   getList,
   create,
   remove,
+  isAllowToDelete,
 };
