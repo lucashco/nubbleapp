8f7c9b7653757304c93c8706787581aa5810c8c5
Lucas Castilho <lucascastilholiveira@gmail.com>
2023-08-18 10:10:31 -0300

    refactor: changes use case to react query

--- src/domain/User/useCases/useUserGetById.ts
+++ src/domain/User/useCases/useUserGetById.ts
@@ -1,32 +1,20 @@
-import {useCallback, useEffect, useState} from 'react';
+import {QueryKeys} from '@infra';
+import {useQuery} from '@tanstack/react-query';
 
 import {userService} from '../userService';
-import {User} from '../userTypes';
 
 export function useUserGetById(id: number) {
-  const [user, setUser] = useState<User>();
-  const [error, setError] = useState<boolean | null>(null);
-  const [loading, setLoading] = useState(false);
-
-  const getUserById = useCallback(async () => {
-    try {
-      setLoading(true);
-      const _user = await userService.getById(id);
-      setUser(_user);
-    } catch (er) {
-      setError(true);
-    } finally {
-      setLoading(false);
-    }
-  }, [id]);
-
-  useEffect(() => {
-    getUserById();
-  }, [getUserById]);
+  const {data, isLoading, isError, refetch, isFetching} = useQuery({
+    queryKey: [QueryKeys.UserGetById, id],
+    queryFn: () => userService.getById(id),
+    staleTime: 1000 * 30, // 30 seconds
+  });
 
   return {
-    user,
-    error,
-    loading,
+    user: data,
+    isError,
+    isLoading,
+    isFetching,
+    refetch,
   };
 }
