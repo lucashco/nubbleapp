64c148bb611b63601e442625388985f4025d95d9
Lucas Castilho <lucascastilholiveira@gmail.com>
2023-08-20 17:24:08 -0300

    feat: adds new query keys

--- src/domain/PostComment/useCases/usePostCommentList.ts
+++ src/domain/PostComment/useCases/usePostCommentList.ts
@@ -1,10 +1,10 @@
 import {PostComment, postCommentService} from '@domain';
-import {usePaginatedList} from '@infra';
+import {QueryKeys, usePaginatedList} from '@infra';
 
 export function usePostCommentList(postId: number) {
   function getList(page: number) {
     return postCommentService.getList(postId, page);
   }
 
-  return usePaginatedList<PostComment>(getList);
+  return usePaginatedList<PostComment>([QueryKeys.PostCommentList], getList);
 }
