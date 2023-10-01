c0c77c8d38326c17ae1514b6b8d94bfd3d04ca1e
Lucas Castilho <lucascastilholiveira@gmail.com>
2023-10-01 10:39:21 -0300

    feat: implements sign out method

--- src/screens/app/SettingsScreen/SettingsScreen.tsx
+++ src/screens/app/SettingsScreen/SettingsScreen.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
+import {useAuthSignOut} from '@domain';
+
 import {Button, Screen, Text} from '@components';
 import {AppScreenProps} from '@routes';
 
 export function SettingsScreen({}: AppScreenProps<'SettingsScreen'>) {
+  const {isLoading, singOut} = useAuthSignOut();
   return (
-    <Screen canGoBack>
+    <Screen canGoBack title="Configurações">
       <Text>SETTINGS SCREEN</Text>
-      <Button title="Home" />
+      <Button loading={isLoading} onPress={singOut} title="Sair da conta" />
     </Screen>
   );
 }
