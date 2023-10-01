cb55d2f0bfece0dca369d2912d28d6de27c4a64b
Lucas Castilho <lucascastilholiveira@gmail.com>
2023-10-01 11:31:11 -0300

    feat: implements credentials state with context

---
+++ src/services/authCredentials/Providers/AuthCredentialsProvider.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {createContext, useState} from 'react';
+
+import {AuthCredentials} from '@domain';
+
+import {AuthCredentialsService} from '../authCredentialsType';
+
+export const AuthCredentialsContext = createContext<AuthCredentialsService>({
+  authCredentials: null,
+  isLoading: true,
+  saveCredentials: async () => {},
+  removeCredentials: async () => {},
+});
+
+export function AuthCredentialsProvider({children}: React.PropsWithChildren) {
+  const [authCredentials, setAuthCredentials] =
+    useState<AuthCredentials | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  async function saveCredentials(ac: AuthCredentials) {
+    setAuthCredentials(ac);
+    setIsLoading(false);
+  }
+
+  async function removeCredentials() {
+    setAuthCredentials(null);
+  }
+
+  return (
+    <AuthCredentialsContext.Provider
+      value={{authCredentials, isLoading, saveCredentials, removeCredentials}}>
+      {children}
+    </AuthCredentialsContext.Provider>
+  );
+}
--- src/services/authCredentials/useAuthCredentials.ts
+++ src/services/authCredentials/useAuthCredentials.ts
@@ -1,15 +1,34 @@
+import {useContext} from 'react';
+
 import {create} from 'zustand';
 import {persist} from 'zustand/middleware';
 
 import {storage} from '../storage';
 
 import {AuthCredentialsService} from './authCredentialsType';
+import {AuthCredentialsContext} from './Providers/AuthCredentialsProvider';
 
 export function useAuthCredentials(): AuthCredentialsService {
-  return useAuthCredentialsZustand();
+  return useAuthCredentialsContext();
 }
 
-const useAuthCredentialsZustand = create<AuthCredentialsService>()(
+const useAuthCredentialsContext = () => {
+  const context = useContext(AuthCredentialsContext);
+
+  if (!context) {
+    throw new Error(
+      'AuthCredentials must be used with AuthCredentialsProvider',
+    );
+  }
+
+  return context;
+};
+
+/**
+ * @deprecated this hook is deprecated and will be removed in a future release
+ */
+
+export const useAuthCredentialsZustand = create<AuthCredentialsService>()(
   persist(
     set => ({
       authCredentials: null,
