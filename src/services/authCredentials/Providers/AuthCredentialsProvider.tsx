cd2d98ce0d8f684693ae588cec20552021aa4552
Lucas Castilho <lucascastilholiveira@gmail.com>
2023-10-02 08:57:55 -0300

    feat: implements storage and context

--- src/services/authCredentials/Providers/AuthCredentialsProvider.tsx
+++ src/services/authCredentials/Providers/AuthCredentialsProvider.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {createContext, useState} from 'react';
 
-import {AuthCredentials} from '@domain';
+import {AuthCredentials, authService} from '@domain';
 
+import {authCredentialsStorage} from '../authCredentialsStorage';
 import {AuthCredentialsService} from '../authCredentialsType';
 
 export const AuthCredentialsContext = createContext<AuthCredentialsService>({
@@ -17,14 +18,36 @@ export function AuthCredentialsProvider({children}: React.PropsWithChildren) {
     useState<AuthCredentials | null>(null);
   const [isLoading, setIsLoading] = useState(true);
 
+  async function startAuthCredentials() {
+    try {
+      const ac = await authCredentialsStorage.get();
+
+      if (ac) {
+        authService.updateToken(ac.token);
+        setAuthCredentials(ac);
+      }
+    } catch (error) {
+      // setAuthCredentials(null);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   async function saveCredentials(ac: AuthCredentials) {
+    authService.updateToken(ac.token);
+    authCredentialsStorage.set(ac);
     setAuthCredentials(ac);
-    setIsLoading(false);
   }
 
   async function removeCredentials() {
+    authService.removeToken();
+    authCredentialsStorage.remove();
     setAuthCredentials(null);
   }
+
+  useEffect(() => {
+    startAuthCredentials();
+  }, []);
 
   return (
     <AuthCredentialsContext.Provider
