2a57a09b22ac588ebb24920373034077ef1f5843
Lucas Castilho <lucascastilholiveira@gmail.com>
2023-10-05 18:01:02 -0300

    feat: implements mmkv

---
+++ src/services/storage/implementation/MMKVStorage.ts
@@ -0,0 +1,23 @@
+import {MMKV} from 'react-native-mmkv';
+
+import {StorageService} from '../storage';
+
+const mmkvInstance = new MMKV();
+
+export const mmkvStorage: StorageService = {
+  getItem: async key => {
+    const item = mmkvInstance.getString(key);
+
+    if (item) {
+      return JSON.parse(item);
+    }
+
+    return null;
+  },
+  setItem: async (key, value) => {
+    mmkvInstance.set(key, JSON.stringify(value));
+  },
+  removeItem: async key => {
+    mmkvInstance.delete(key);
+  },
+};
