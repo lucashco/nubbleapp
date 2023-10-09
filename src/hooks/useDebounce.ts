23ba22a15cb47b84aa568814021f4b4cb4988696
Lucas Castilho <lucascastilholiveira@gmail.com>
2023-10-09 08:13:33 -0300

    refact: improves debouce hook

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -8,11 +8,11 @@ import {useEffect, useState} from 'react';
  * @returns debounced value
  */
 
-export function useDebounce<T>(value: T, delay?: number): T {
+export function useDebounce<T>(value: T, delay = 500): T {
   const [debouncedValue, setDebouncedValue] = useState<T>(value);
 
   useEffect(() => {
-    const timer = setTimeout(() => setDebouncedValue(value), delay || 500);
+    const timer = setTimeout(() => setDebouncedValue(value), delay);
 
     return () => {
       clearTimeout(timer);
