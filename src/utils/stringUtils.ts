ae86f88480371d259aaa2818795a4600d05ad9eb
Lucas Castilho <lucascastilholiveira@gmail.com>
2023-10-09 06:53:27 -0300

    refact: abstract capitalize function

---
+++ src/utils/stringUtils.ts
@@ -0,0 +1,10 @@
+function capitalizeFirsLetter(value: string): string {
+  return value
+    .split(' ')
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+    .join(' ');
+}
+
+export const stringUtils = {
+  capitalizeFirsLetter,
+};
