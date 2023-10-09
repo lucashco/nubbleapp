1ba8ed85e1ba04a252d1208dbffcc221f04179d1
Lucas Castilho <lucascastilholiveira@gmail.com>
2023-10-09 07:57:16 -0300

    feat: creates debouce hook

---
+++ src/hooks/useDebounce.ts
@@ -0,0 +1,23 @@
+import {useEffect, useState} from 'react';
+
+/**
+ *
+ * @description https://usehooks-ts.com/react-hook/use-debounce
+ * @param value to be debounced
+ * @param delay in milliseconds
+ * @returns debounced value
+ */
+
+export function useDebounce<T>(value: T, delay?: number): T {
+  const [debouncedValue, setDebouncedValue] = useState<T>(value);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setDebouncedValue(value), delay || 500);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+}
