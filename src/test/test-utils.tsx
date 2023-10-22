08ed2f6935e7e96ce39a7d0dcc879f31fa56f86b
Lucas Castilho <lucascastilholiveira@gmail.com>
2023-10-22 10:22:42 -0300

    feat: creates own custom render

---
+++ src/test/test-utils.tsx
@@ -0,0 +1,21 @@
+import React, {PropsWithChildren, ReactElement} from 'react';
+
+import {ThemeProvider} from '@shopify/restyle';
+import {RenderOptions, render} from '@testing-library/react-native';
+
+import {theme} from '@theme';
+
+const AllTheProviders = ({children}: PropsWithChildren) => {
+  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+};
+
+const customRender = <T extends unknown>(
+  component: ReactElement<T>,
+  options?: Omit<RenderOptions, 'wrapper'>,
+) => render(component, {wrapper: AllTheProviders, ...options});
+
+// re-export everything
+export * from '@testing-library/react-native';
+
+// override render method
+export {customRender as render};
