5073bef954c36b82bbe5c708a2ecaec8e86fc22a
Lucas Castilho <lucascastilholiveira@gmail.com>
2023-10-22 11:54:25 -0300

    chore: adds collect coverage  from

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,8 @@ module.exports = {
   preset: 'react-native',
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
   moduleDirectories: ['node_modules', './src/test'],
+  collectCoverageFrom: [
+    'src/{components,utils}/**/*.{js,jsx,ts,tsx}',
+    '!**/node_modules/**',
+  ],
 };
