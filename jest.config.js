29edefd0389a9db397f82f92c177232204496c17
Lucas Castilho <lucascastilholiveira@gmail.com>
2023-10-29 15:36:07 -0300

    test: creates navigation global mock

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   preset: 'react-native',
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
   moduleDirectories: ['node_modules', './src/test'],
+  setupFiles: ['<rootDir>/src/test/jestSetup.ts'],
   modulePathIgnorePatterns: ['.*/mockedData/.*'],
   collectCoverageFrom: [
     'src/{components,utils}/**/*.{js,jsx,ts,tsx}',
