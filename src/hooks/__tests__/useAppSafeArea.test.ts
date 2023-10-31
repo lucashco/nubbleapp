b929953d8b4823ed812d1ae2f1d94a03012392ac
Lucas Castilho <lucascastilholiveira@gmail.com>
2023-10-31 19:36:14 -0300

    test: creates test for the use app safe area

---
+++ src/hooks/__tests__/useAppSafeArea.test.ts
@@ -0,0 +1,39 @@
+import {renderHook} from '@testing-library/react-native';
+import {useSafeAreaInsets, EdgeInsets} from 'react-native-safe-area-context';
+import {AllTheProviders} from 'test-utils';
+
+import {theme} from '@theme';
+
+import {useAppSafeArea} from '../useAppSafeArea';
+
+jest.mock('react-native-safe-area-context');
+
+const mockedUseSafeAreaInsets = jest.mocked(useSafeAreaInsets);
+
+describe('useAppSafeArea', () => {
+  it('should returns the minimum spacement required', () => {
+    mockedUseSafeAreaInsets.mockImplementation(
+      () => ({top: 5, bottom: 5} as EdgeInsets),
+    );
+
+    const {result} = renderHook(() => useAppSafeArea(), {
+      wrapper: AllTheProviders,
+    });
+
+    expect(result.current.top).toEqual(theme.spacing.s20);
+    expect(result.current.bottom).toEqual(theme.spacing.s20);
+  });
+
+  it('should returns the safe area spacement', () => {
+    mockedUseSafeAreaInsets.mockImplementation(
+      () => ({top: 40, bottom: 40} as EdgeInsets),
+    );
+
+    const {result} = renderHook(() => useAppSafeArea(), {
+      wrapper: AllTheProviders,
+    });
+
+    expect(result.current.top).toBeGreaterThan(theme.spacing.s20);
+    expect(result.current.bottom).toBeGreaterThan(theme.spacing.s20);
+  });
+});
