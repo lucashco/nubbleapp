cdc2c228f32b859d83576387489f6e6d87d78c7b
Lucas Castilho <lucascastilholiveira@gmail.com>
2023-11-10 09:33:29 -0300

    feat: creates new handler

--- src/test/server/postComment/postCommentHandlers.ts
+++ src/test/server/postComment/postCommentHandlers.ts
@@ -6,11 +6,31 @@ import {mockedData} from './mocks';
 
 const url = `${BASE_URL}${POST_COMMENT_BASE_URL}`;
 
+let inMemoryReponse = {...mockedData.mockedPostCommentResponse};
+
 export const postCommentHandlers = [
   http.get(url, async () => {
-    const response: PageAPI<PostCommentAPI> =
-      mockedData.mockedPostCommentResponse;
+    const response: PageAPI<PostCommentAPI> = inMemoryReponse;
 
     return HttpResponse.json(response, {status: 200});
+  }),
+
+  http.post<any, {post_id: number; message: string}>(url, async ({request}) => {
+    const body = await request.json();
+
+    const newPostCommentAPI: PostCommentAPI = {
+      ...mockedData.postCommentAPI,
+      id: 999,
+      post_id: body.post_id,
+      message: body.message,
+    };
+
+    inMemoryReponse.data = [newPostCommentAPI, ...inMemoryReponse.data];
+    inMemoryReponse.meta = {
+      ...inMemoryReponse.meta,
+      total: inMemoryReponse.meta.total + 1,
+    };
+
+    return HttpResponse.json(newPostCommentAPI, {status: 201});
   }),
 ];
