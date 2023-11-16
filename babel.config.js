ca1cb61dddec94122aad1cb22a03b3475c9802d3
Lucas Castilho <lucascastilholiveira@gmail.com>
2023-11-16 08:47:28 -0300

    chore: adjusts on babel file

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@ module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
     [
-      'module-resolver',
+      require.resolve('babel-plugin-module-resolver'),
       {
         root: '.',
         alias: {
