8bee2d25d570b2fd180a0fad28496d77158b5f14
Lucas Castilho <lucascastilholiveira@gmail.com>
2023-11-16 06:35:23 -0300

    test: adds new handler

--- src/test/server/postComment/postCommentHandlers.ts
+++ src/test/server/postComment/postCommentHandlers.ts
@@ -1,12 +1,17 @@
 import {BASE_URL, PageAPI} from '@api';
 import {PostCommentAPI, POST_COMMENT_BASE_URL} from '@domain';
+import cloneDeep from 'lodash/cloneDeep';
 import {http, HttpResponse} from 'msw';
 
 import {mockedData} from './mocks';
 
 const url = `${BASE_URL}${POST_COMMENT_BASE_URL}`;
 
-let inMemoryReponse = {...mockedData.mockedPostCommentResponse};
+let inMemoryReponse = cloneDeep(mockedData.mockedPostCommentResponse);
+
+export function resetInMemoryReponse() {
+  inMemoryReponse = cloneDeep(mockedData.mockedPostCommentResponse);
+}
 
 export const postCommentHandlers = [
   http.get(url, async () => {
@@ -33,4 +38,21 @@ export const postCommentHandlers = [
 
     return HttpResponse.json(newPostCommentAPI, {status: 201});
   }),
+
+  http.delete<{postCommentId: string}>(
+    `${url}/:postCommentId`,
+    async ({params}) => {
+      const postCommentId = params.postCommentId;
+
+      inMemoryReponse.data = inMemoryReponse.data.filter(
+        item => item.id.toString() !== postCommentId,
+      );
+      inMemoryReponse.meta = {
+        ...inMemoryReponse.meta,
+        total: inMemoryReponse.meta.total - 1,
+      };
+
+      return HttpResponse.json({message: 'removed'}, {status: 200});
+    },
+  ),
 ];
--- src/test/server/server.ts
+++ src/test/server/server.ts
@@ -3,3 +3,7 @@ import {setupServer} from 'msw/node';
 import {postCommentHandlers} from './postComment/postCommentHandlers';
 
 export const server = setupServer(...postCommentHandlers);
+
+export {mockedData as mockedPostComment} from './postComment/mocks';
+
+export {resetInMemoryReponse} from './postComment/postCommentHandlers';
