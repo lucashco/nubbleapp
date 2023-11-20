e2ad7e16374271f43efad71a8f61604306a02d92
Lucas Castilho <lucascastilholiveira@gmail.com>
2023-11-20 10:34:14 -0300

    feat: adds text input on the search screen

--- src/screens/app/SearchScreen/SearchScreen.tsx
+++ src/screens/app/SearchScreen/SearchScreen.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-import {Screen, Text} from '@components';
+import {Icon, Screen, Text, TextInput} from '@components';
 
 export function SearchScreen() {
+  const [search, setSearch] = useState('');
   return (
-    <Screen>
+    <Screen
+      canGoBack
+      HeaderComponent={
+        <TextInput
+          boxProps={{
+            flex: 1,
+            flexShrink: 1,
+            marginLeft: 's10',
+          }}
+          LeftComponent={<Icon name={'search'} color="gray3" />}
+          value={search}
+          onChangeText={setSearch}
+        />
+      }>
       <Text>Search Screen</Text>
     </Screen>
   );
