7471470ba30b22c5bbcb1651ef86152858cbe882
Lucas Castilho <lucascastilholiveira@gmail.com>
2023-11-20 08:41:09 -0300

    feat: setup search screen

---
+++ src/screens/app/SearchScreen/SearchScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import {Screen, Text} from '@components';
+
+export function SearchScreen() {
+  return (
+    <Screen>
+      <Text>Search Screen</Text>
+    </Screen>
+  );
+}
