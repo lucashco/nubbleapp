8c930b0103b45730edeccedf3e0edccd4ec5c5ea
Lucas Castilho <lucascastilholiveira@gmail.com>
2024-01-27 09:37:50 -0300

    feat: finishes publish post screen

--- src/screens/app/PublishPostScreen/PublishPostScreen.tsx
+++ src/screens/app/PublishPostScreen/PublishPostScreen.tsx
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Dimensions, Image, ImageStyle, StyleProp} from 'react-native';
 
-import {Screen, Text} from '@components';
+import {Button, Screen, Text, TextInput} from '@components';
 import {AppScreenProps} from '@routes';
 
-export function PublishPostScreen({}: AppScreenProps<'PublishPostScreen'>) {
+const IMAGE_WIDTH = Dimensions.get('screen').width / 2; // 50% of the screen width
+
+export function PublishPostScreen({
+  route,
+}: AppScreenProps<'PublishPostScreen'>) {
+  const [description, setDescription] = useState('');
+
+  const {imageUri} = route.params;
   return (
-    <Screen>
-      <Text>Olá publish screen</Text>
+    <Screen scrollable canGoBack title="Novo post">
+      <Image source={{uri: imageUri}} style={$imageStyle} />
+      <Text preset="headingSmall" mt="s32" mb="s10">
+        Escreva uma legenda
+      </Text>
+      <TextInput
+        value={description}
+        onChangeText={setDescription}
+        placeholder="Digite aqui"
+        containerProps={{
+          borderWidth: 0,
+        }}
+      />
+      <Button mt="s56" title="Publicar post" preset="primary" />
     </Screen>
   );
 }
+
+const $imageStyle: StyleProp<ImageStyle> = {
+  width: IMAGE_WIDTH,
+  height: IMAGE_WIDTH,
+  alignSelf: 'center',
+  marginTop: 20,
+};
